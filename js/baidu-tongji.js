47fb8d1e0b57210bfe292e42baac5a1e5a4acc35
JimiZhou <jayvzhou@gmail.com>
2020-04-23 16:14:02 +0800

    Site updated: 2020-04-23 16:13:55

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a3e19fd98798088b8ee65729e23d3729";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
